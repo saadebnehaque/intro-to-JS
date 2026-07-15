233489bcf1065271240c5a4f89320ba90536f522
Saad <sebnehaque@gmail.com>
2026-07-15 19:16:35 +0600

    change something

--- add.js
+++ add.js
@@ -3,19 +3,19 @@ const mangoPrice = 20;
 const totalPrice = orangePrice + mangoPrice;
 // console.log(typeof orangePrice);
 // console.log(typeof mangoPrice);
-// console.log(totalPrice);
+console.log(totalPrice);
 
 const firstName = 'Ananta';
 const lastName = 'Jalil';
-const fullName = firstName + lastName;
-// console.log(fullName);
+const fullName = firstName + ' ' + lastName;
+console.log(fullName);
 
 const numberString = '20.3';
-// const number = parseInt(numberString)
-const number = parseFloat(numberString)
+const number = parseInt(numberString)
+// const number = parseFloat(numberString)
 // const 
 // console.log(typeof number)
-// console.log(number)
+console.log(number)
 
 const firstNumber = 0.1;
 const secondNumber = 0.2;
