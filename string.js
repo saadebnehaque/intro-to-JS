5c5b4daf05ac1852241322e02645035df3c537f9
Saad <sebnehaque@gmail.com>
2026-07-15 16:52:07 +0600

    change something

--- string.js
+++ string.js
@@ -1,6 +1,10 @@
 let name = "kalam";
-console.log(name);
+console.log('Name: ' + name);
 
 let country = "Bangladesh";
+console.log('Country: ' + country);
+
+let city = "Dhaka";
+console.log('City: ' + city);
 let learning = 'JavaScript Programming  language. another thing.';
-console.log(learning);
+console.log('Learning: ' + learning);
